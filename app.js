dbc720b4ab29b7368632dd901d3499cdf3bf252b
Diana <blackpyjac@localhost.localdomain>
2023-05-08 23:52:13 -0600

    new changes test 1.2

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use('/public',express.static('public'));
+/* app.use('/public',express.static('public')); */
+app.use(express.static(path.join(__dirname, 'public')))
+
 /* app.use(express.static("public")); */
 
 app.get("/", (req, res) => {
