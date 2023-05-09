2d640cb56dbd62e4b15c2eff4ee43bf94ba80477
Diana <blackpyjac@localhost.localdomain>
2023-05-08 22:22:37 -0600

    the app.js was modified

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
-/* app.use('/public',express.static('public')); */
-app.use(express.static("public"));
+app.use('/public',express.static('public'));
+/* app.use(express.static("public")); */
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/signup.html");
